8c047c1fa46a2aa74a07c166ca7f64e952ddc25b
Lucas Frota <lucasmigfrota16@gmail.com>
2024-08-07 14:46:09 -0300

    Update supabase.js

--- correiosFront/src/app/supabase.js
+++ correiosFront/src/app/supabase.js
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://szmxzwdyhhptgcmdocol.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6bXh6d2R5aGhwdGdjbWRvY29sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY4MzA1MTYsImV4cCI6MjAzMjQwNjUxNn0.u8ud3YilsqgkaJAwYyX02lfpKqNVA0-1AZhprvU1UG4"
+const supabaseAnonKey = "{{{{aqui vai sua chave anonima }}}}"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
